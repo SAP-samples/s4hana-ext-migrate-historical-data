3eab3574ea33c476bb24e3149b7d706c74298e0d
amritanshu kumar <amritanshukumar93@gmail.com>
2024-07-31 08:51:34 +0530

    Delete ui/WebContent directory

--- ui/WebContent/controller/App.controller.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define([
-	"sap/migratedsalesorder/controller/BaseController",
-   "sap/m/MessageToast"
-], function (BaseController, MessageToast) {
-   "use strict";
-   return BaseController.extend("sap.migratedsalesorder.controller.App", {
-   });
-});
